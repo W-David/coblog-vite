60000d6593f941badc271d3f41e33512f69ecc3b
DBH\502208 <zhenhuawang@insigmaservice.com>
2024-07-02 18:34:48 +0800

    📃 docs: 配置更新

---
+++ apollo.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	client: {
+		service: {
+			name: 'coblog-graphql',
+			url: 'http://localhost:3000/graphql',
+			headers: {
+				authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicm9vdCIsImVtYWlsIjoicm9vdEByb290LmNvbSIsInJvbGUiOiJST09UIiwiaWF0IjoxNzE5NDUzNTcxLCJleHAiOjE3MjIwNDU1NzF9.FcCRV9-DbLxIp7EmXNuO7QiQV6TlVRxam3Kgpx-HaX4'
+			}
+		},
+		includes: ['src/**/*.vue', 'src/**/*.ts']
+	},
+}
--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	client: {
-		service: {
-			name: 'coblog-graphql',
-			url: 'http://localhost:3000/graphql'
-		}
-	},
-	includes: ['src/**/*.vue', 'src/**/*.ts']
-}
