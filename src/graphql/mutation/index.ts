3487f930604c9ce6b62b2a029d6e7f93c25e7332
DBH\502208 <zhenhuawang@insigmaservice.com>
2024-07-03 09:34:26 +0800

    ✨ feat: graphql api update

---
+++ src/graphql/mutation/index.ts
@@ -0,0 +1,107 @@
+export const register = gql`
+	mutation Register($email: String!, $password: String!, $role: String!) {
+		register(email: $email, password: $password, role: $role) {
+			token
+		}
+	}
+`
+
+export const deleteOneUser = gql`
+	mutation DeleteOneUser($where: UserWhereUniqueInput!) {
+		deleteOneUser(where: $where) {
+			id
+			email
+			nickName
+			avatar
+		}
+	}
+`
+
+export const updateOneUser = gql`
+	mutation UpdateOneUser($data: UserUpdateInput!, $where: UserWhereUniqueInput!) {
+		updateOneUser(data: $data, where: $where) {
+			id
+			email
+			nickName
+			avatar
+			role
+		}
+	}
+`
+
+export const createOnePost = gql`
+	mutation CreateOnePost($data: PostCreateInput!) {
+		createOnePost(data: $data) {
+			id
+			title
+			authorId
+		}
+	}
+`
+
+export const updateOnePost = gql`
+	mutation UpdateOnePost($data: PostUpdateInput!, $where: PostWhereUniqueInput!) {
+		updateOnePost(data: $data, where: $where) {
+			id
+		}
+	}
+`
+
+export const createOneCategory = gql`
+	mutation CreateOneCategory($data: CategoryCreateInput!) {
+		createOneCategory(data: $data) {
+			id
+			name
+		}
+	}
+`
+
+export const deleteOnePost = gql`
+	mutation DeleteOnePost($where: PostWhereUniqueInput!) {
+		deleteOnePost(where: $where) {
+			id
+			name
+		}
+	}
+`
+
+export const deleteOneCategory = gql`
+	mutation DeleteOneCategory($where: CategoryWhereUniqueInput!) {
+		deleteOneCategory(where: $where) {
+			id
+			name
+		}
+	}
+`
+
+export const updateOneTag = gql`
+	mutation UpdateOneTag($data: TagUpdateInput!, $where: TagWhereUniqueInput!) {
+		updateOneTag(data: $data, where: $where) {
+			id
+			name
+		}
+	}
+`
+
+export const deleteOneTag = gql`
+	mutation DeleteOneTag($where: TagWhereUniqueInput!) {
+		deleteOneTag(where: $where) {
+			id
+			name
+		}
+	}
+`
+
+export const search = gql`
+	query Search($where: PostWhereInput, $categoriesWhere: CategoryWhereInput, $tagsWhere: TagWhereInput) {
+		posts(where: $where) {
+			title
+		}
+		categories(where: $categoriesWhere) {
+			name
+		}
+		tags(where: $tagsWhere) {
+			name
+		}
+	}
+`
---
+++ src/graphql/query/index.ts
@@ -0,0 +1,136 @@
+export const login = gql`
+	query Login($email: String!, $password: String!) {
+		auth(email: $email, password: $password) {
+			token
+		}
+	}
+`
+
+export const users = gql`
+	query Users {
+		users {
+			email
+			id
+			nickName
+			role
+			avatar
+		}
+	}
+`
+
+export const getUser = gql`
+	query GetUser($where: UserWhereUniqueInput!) {
+		getUser(where: $where) {
+			id
+			email
+			avatar
+			role
+			nickName
+			createdAt
+		}
+	}
+`
+
+export const getPost = gql`
+	query GetPost($where: PostWhereUniqueInput!) {
+		getPost(where: $where) {
+			title
+			content
+			browNum
+			published
+			updatedAt
+			createdAt
+			description
+			favoNum
+			authorId
+			author {
+				nickName
+				email
+				createdAt
+				role
+			}
+			id
+		}
+	}
+`
+
+export const posts = gql`
+	query Posts($orderBy: [PostOrderByWithRelationInput!]) {
+		posts(orderBy: $orderBy) {
+			id
+			createdAt
+			updatedAt
+			title
+			description
+			content
+			published
+			favoNum
+			browNum
+			authorId
+		}
+	}
+`
+
+export const categories = gql`
+	query Categories($where: CategoryWhereInput) {
+		categories(where: $where) {
+			id
+			name
+		}
+	}
+`
+
+export const getCategory = gql`
+	query GetCategory($where: CategoryWhereUniqueInput!) {
+		getCategory(where: $where) {
+			name
+			id
+			CategoriesOnPosts {
+				post {
+					id
+					createdAt
+					updatedAt
+					title
+					description
+					content
+					published
+					favoNum
+					browNum
+					authorId
+				}
+			}
+		}
+	}
+`
+
+export const tags = gql`
+	query Tags {
+		tags {
+			id
+			name
+		}
+	}
+`
+
+export const getTag = gql`
+	query GetTag($where: TagWhereUniqueInput!) {
+		getTag(where: $where) {
+			name
+			id
+			TagsOnPosts {
+				post {
+					id
+					createdAt
+					updatedAt
+					title
+					description
+					content
+					published
+					favoNum
+					browNum
+					authorId
+				}
+			}
+		}
+	}
+`
